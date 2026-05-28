11eee72ac283d8c90e9f0343bec0a9f2d563b1e2
saulocaceresg <scaceresg@unsa.edu.pe>
2026-05-28 12:28:41 -0500

    relación de oficina a oficina_representate corregida

--- empresa_distribucion_jandy.sql
+++ empresa_distribucion_jandy.sql
@@ -112,8 +112,7 @@ CREATE TABLE IF NOT EXISTS public.movimiento
 CREATE TABLE IF NOT EXISTS public.oficina
 (
     ofiid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    ofirepid integer NOT NULL,
-    -- empid integer NOT NULL,
+    empid integer NOT NULL,
     ofirepnom character varying(50) COLLATE pg_catalog."default" NOT NULL,
     ofirepestreg boolean NOT NULL,
     CONSTRAINT oficina_pkey PRIMARY KEY (ofiid)
