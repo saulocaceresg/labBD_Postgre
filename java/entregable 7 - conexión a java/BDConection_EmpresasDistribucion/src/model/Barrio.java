e0170907bb6f3ac64c4c69ac969995278d1c58db
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:08:44 -0500

    clase barrio

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Barrio.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Barrio.java
@@ -2,7 +2,52 @@ package model;
 
 public class Barrio {
 	
+	private int barid;
+	private String barnom;
+	private String bardes;
+	private String barestreg;
+	
 	public Barrio() {
 	}
+	
+	public Barrio (int barid, String barnom, String bardes, String barestreg) {
+		this.barid = barid;
+		this.barnom = barnom;
+		this.bardes = bardes;
+		this.barestreg = barestreg;
+	}
+
+	public int getBarid() {
+		return barid;
+	}
+
+	public void setBarid(int barid) {
+		this.barid = barid;
+	}
+
+	public String getBarnom() {
+		return barnom;
+	}
+
+	public void setBarnom(String barnom) {
+		this.barnom = barnom;
+	}
+
+	public String getBardes() {
+		return bardes;
+	}
+
+	public void setBardes(String bardes) {
+		this.bardes = bardes;
+	}
+
+	public String getBarestreg() {
+		return barestreg;
+	}
+
+	public void setBarestreg(String barestreg) {
+		this.barestreg = barestreg;
+	}
+	
 	
 }
