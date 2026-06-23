74ed2f58762af8fba79f5f6e533f9500c0a4be34
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:17:28 -0500

    clase ciudad

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Ciudad.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Ciudad.java
@@ -1,5 +1,62 @@
 package model;
 
 public class Ciudad {
+	private int ciuid;
+	private int regid; // Clave foránea de Region
+	private String ciunom;
+	private String ciudes;
+	private String ciuestreg;
+	
+	public Ciudad() {
+	}
+	
+	public Ciudad(int ciuid, int regid, String ciunom, String ciudes, String ciuestreg) {
+		this.ciuid = ciuid;
+		this.regid = regid;
+		this.ciunom = ciunom;
+		this.ciudes = ciudes;
+		this.ciuestreg = ciuestreg;
+	}
 
+	public int getCiuid() {
+		return ciuid;
+	}
+
+	public void setCiuid(int ciuid) {
+		this.ciuid = ciuid;
+	}
+
+	public int getRegid() {
+		return regid;
+	}
+
+	public void setRegid(int regid) {
+		this.regid = regid;
+	}
+
+	public String getCiunom() {
+		return ciunom;
+	}
+
+	public void setCiunom(String ciunom) {
+		this.ciunom = ciunom;
+	}
+
+	public String getCiudes() {
+		return ciudes;
+	}
+
+	public void setCiudes(String ciudes) {
+		this.ciudes = ciudes;
+	}
+
+	public String getCiuestreg() {
+		return ciuestreg;
+	}
+
+	public void setCiuestreg(String ciuestreg) {
+		this.ciuestreg = ciuestreg;
+	}
+	
+	
 }
