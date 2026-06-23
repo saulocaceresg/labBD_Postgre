79fc8e652f191bf50c719cd03de644afc42f8c8f
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:31:23 -0500

    clase unidadmedida

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/UnidadMedida.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/UnidadMedida.java
@@ -1,5 +1,41 @@
 package model;
 
 public class UnidadMedida {
+	private String uniid;
+	private String uninom;
+	private String uniestreg;
+	
+	public UnidadMedida() {
+	}
+	
+	public UnidadMedida(String unid, String uninom, String uniestreg) {
+		this.uniid = unid;
+		this.uninom = uninom;
+		this.uniestreg = uniestreg;
+	}
 
+	public String getUniid() {
+		return uniid;
+	}
+
+	public void setUniid(String uniid) {
+		this.uniid = uniid;
+	}
+
+	public String getUninom() {
+		return uninom;
+	}
+
+	public void setUninom(String uninom) {
+		this.uninom = uninom;
+	}
+
+	public String getUniestreg() {
+		return uniestreg;
+	}
+
+	public void setUniestreg(String uniestreg) {
+		this.uniestreg = uniestreg;
+	}
+	
 }
