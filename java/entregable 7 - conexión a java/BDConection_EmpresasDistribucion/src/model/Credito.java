6cc50086c84a4670fc62060d58f56a1c56433f2d
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:27:14 -0500

    clase credito

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Credito.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Credito.java
@@ -1,5 +1,53 @@
 package model;
 
 public class Credito {
+	private byte creid;
+	private String crenom;
+	private String credes;
+	private String creestreg;
+	
+	public Credito() {
+	}
+	
+	public Credito(byte creid, String crenom, String credes, String creestreg) {
+		this.creid = creid;
+		this.crenom = crenom;
+		this.credes = credes;
+		this.creestreg = creestreg;
+	}
 
+	public byte getCreid() {
+		return creid;
+	}
+
+	public void setCreid(byte creid) {
+		this.creid = creid;
+	}
+
+	public String getCrenom() {
+		return crenom;
+	}
+
+	public void setCrenom(String crenom) {
+		this.crenom = crenom;
+	}
+
+	public String getCredes() {
+		return credes;
+	}
+
+	public void setCredes(String credes) {
+		this.credes = credes;
+	}
+
+	public String getCreestreg() {
+		return creestreg;
+	}
+
+	public void setCreestreg(String creestreg) {
+		this.creestreg = creestreg;
+	}
+	
+	
+	
 }
