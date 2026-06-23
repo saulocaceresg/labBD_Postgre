a64e1b39353563e397554c52644ae1a0e5732f32
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:39:55 -0500

    clase tipoempresa

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/TipoEmpresa.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/TipoEmpresa.java
@@ -1,5 +1,41 @@
 package model;
 
 public class TipoEmpresa {
+	private int tipid;
+	private String tipnom;
+	private String tipestreg;
+	
+	public TipoEmpresa() {
+	}
+	
+	public TipoEmpresa(int tipid, String tipnom, String tipestreg) {
+		this.tipid = tipid;
+		this.tipnom = tipnom;
+		this.tipestreg = tipestreg;
+	}
 
+	public int getTipid() {
+		return tipid;
+	}
+
+	public void setTipid(int tipid) {
+		this.tipid = tipid;
+	}
+
+	public String getTipnom() {
+		return tipnom;
+	}
+
+	public void setTipnom(String tipnom) {
+		this.tipnom = tipnom;
+	}
+
+	public String getTipestreg() {
+		return tipestreg;
+	}
+
+	public void setTipestreg(String tipestreg) {
+		this.tipestreg = tipestreg;
+	}
+	
 }
