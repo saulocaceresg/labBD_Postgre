412326f7b5f43fbbdee78059bd62d1688185b054
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-23 09:33:23 -0500

    clase categoria

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Categoria.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/model/Categoria.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Categoria {
+	private int catid;
+	private String catnom;
+	private String catestreg;
+	
+	public Categoria() {
+	}
+	
+	public Categoria(int catid, String catnom, String catestreg) {
+		this.catid = catid;
+		this.catnom = catnom;
+		this.catestreg = catestreg;
+	}
 
+	public int getCatid() {
+		return catid;
+	}
+
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+
+	public String getCatnom() {
+		return catnom;
+	}
+
+	public void setCatnom(String catnom) {
+		this.catnom = catnom;
+	}
+
+	public String getCatestreg() {
+		return catestreg;
+	}
+
+	public void setCatestreg(String catestreg) {
+		this.catestreg = catestreg;
+	}
+	
 }
