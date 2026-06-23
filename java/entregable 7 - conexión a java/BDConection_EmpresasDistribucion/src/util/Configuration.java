cef2b4143b805d0de7c1bf3d37369b905b209899
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-22 22:50:30 -0500

    configuración y conexión a la bd

--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/util/Configuration.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/util/Configuration.java
@@ -3,6 +3,8 @@ package util;
 import java.util.ResourceBundle;
 
 public class Configuration {
+
+	// ÚNICAMENTE lee las credenciales de la bd 
 	public static String getProperties(String cipher) {
 		ResourceBundle rscBundle = ResourceBundle.getBundle("bd");
 		return rscBundle.getString(cipher);
--- java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/util/ConnectionFactory.java
+++ java/entregable 7 - conexión a java/BDConection_EmpresasDistribucion/src/util/ConnectionFactory.java
@@ -1,5 +1,19 @@
 package util;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+	
+	// Rehusado de ConnectionDB y obtenido de Configuration
+	private static final String URL = Configuration.getProperties("URL");
+	private static final String USER = Configuration.getProperties("USER");
+	private static final String PASSWORD = Configuration.getProperties("PASSWORD");
+	
+	// Método para conectar con al BD. Será usado por los DAO.
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+	
 }
