ad4b5f497aef1904f4eaa6a24c7582b0d924b607
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-30 00:42:24 -0500

    método para añadir registros

--- java/app/EmpresasDistribucion/src/dao/CargoDAO.java
+++ java/app/EmpresasDistribucion/src/dao/CargoDAO.java
@@ -17,7 +17,7 @@ public class CargoDAO extends BaseDAO {
 			ORDER BY carid
 			""";
 	private static final String ADD = """
-			INSERTO INTO cargo (carnom, cardes, carsue, carestreg) VALUES (?, ?, ?, ?)
+			INSERT INTO cargo (carnom, cardes, carsue, carestreg) VALUES (?, ?, ?, ?)
 			""";
 	private static final String MODIFY = """
 			UPDATE cargo
@@ -28,10 +28,9 @@ public class CargoDAO extends BaseDAO {
 //			
 //			""";
 	
+	// list para listar los ítemes en la app
 	public ArrayList<Cargo> list() throws SQLException {
 		ArrayList<Cargo> items = new ArrayList<Cargo>();
-		
-//		String sql = LIST;
 		
 		try (
 				Connection cn = getConnection(); // Se conecta a la BD con el método de ConnectionFactory
@@ -56,6 +55,22 @@ public class CargoDAO extends BaseDAO {
 		}
 		
 		return items;
+	}
+	
+	public void add(Cargo cargo) throws SQLException {
+		try (
+				Connection cn = getConnection();
+				PreparedStatement ps = prepareStatement(cn, ADD);
+				) {
+			ps.setString(1, cargo.getCarnom());
+			ps.setString(2, cargo.getCardes());
+			ps.setBigDecimal(3, cargo.getCarsue());
+			ps.setString(4, cargo.getCarestreg());
+			
+			ps.executeUpdate();	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
