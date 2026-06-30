1fde69cb7f94d3700442c10a1ff542b2989768f7
saulocaceresg <scaceresg@unsa.edu.pe>
2026-06-30 00:40:34 -0500

    controller para la obtención de datos

--- java/app/EmpresasDistribucion/src/controller/CargoController.java
+++ java/app/EmpresasDistribucion/src/controller/CargoController.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class CargoController {
+import java.math.BigDecimal;
 
+import dao.*;
+import model.*;
+import view.*;
+
+public class CargoController {
+	
+	private CargoForm cargoForm;
+	private CargoDAO cargoDAO;
+	
+	public CargoController(CargoForm form) {
+		this.cargoForm = form;
+		this.cargoDAO = new CargoDAO();
+	}
+	
+	public void add() {
+		try {
+			Cargo cargo = new Cargo();
+			
+			cargo.setCarnom(cargoForm.getCarnom());
+			cargo.setCardes(cargoForm.getCardes());
+			cargo.setCarsue(new BigDecimal(cargoForm.getCarsue()));
+			cargo.setCarestreg(cargoForm.getCarestreg());
+			
+			cargoDAO.add(cargo);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
